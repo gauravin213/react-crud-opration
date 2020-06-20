b81f9f4fb360f9e2066c015ff1bf52940b2becb4
gaurav <gauravin213@gmail.com>
2020-06-20 22:55:56 +0530

    Updates

---
+++ precache-manifest.5888f9ceff558fc143d7197897e36094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a1f85559e8189f01e9805d75554319",
+    "url": "/react-crud-opration/index.html"
+  },
+  {
+    "revision": "72803ad531245b4efe1f",
+    "url": "/react-crud-opration/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0b90cafc71bafbb295f5",
+    "url": "/react-crud-opration/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "72803ad531245b4efe1f",
+    "url": "/react-crud-opration/static/js/2.9a78033a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-crud-opration/static/js/2.9a78033a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b90cafc71bafbb295f5",
+    "url": "/react-crud-opration/static/js/main.3c064565.chunk.js"
+  },
+  {
+    "revision": "ed54f75cf39a519fe6a9",
+    "url": "/react-crud-opration/static/js/runtime-main.0942eae9.js"
+  }
+]);
